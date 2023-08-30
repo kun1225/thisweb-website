4a82e3d0832c7ed302fda537274f54d7f912f15f
thisWeb1225 <shan881225@gmail.com>
2023-08-31 01:41:40 +0800

    🔧 Conf: ignore some warning

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true };
+const nextConfig = { 
+  reactStrictMode: true, 
+  swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+};
 
 module.exports = withContentlayer(nextConfig);
