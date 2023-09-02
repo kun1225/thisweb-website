3c54045ddabf273876405c467c360631c7e17d94
thisWeb1225 <shan881225@gmail.com>
2023-09-02 18:52:19 +0800

    :sparkles: Feat: add Magnetic component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Magnetic from "./effect/Magnetic"
 
 export const navContent = [
   {
@@ -14,14 +15,23 @@ export const navContent = [
 const Header = () => {
 
   return (
-    <header className="flex justify-between items-center px-4 md:px-8 py-4 shadow-sm shadow-gray-200 sticky top-0 backdrop-blur-md z-50 bg-[#ffffff85]">
-      <Link href="/"><h1 className="font-tw-code">ThisWeb.</h1></Link>
+    <header className="flex justify-between items-center px-4 md:px-8 py-2 shadow-sm shadow-gray-200 sticky top-0 backdrop-blur-md z-50 bg-[#ffffff85]">
+      <Link href="/">
+        <Magnetic className="p-2">
+          <h1 className="font-tw-code">ThisWeb.</h1>
+        </Magnetic>
+      </Link>
       <nav>
         <ul className="text-xs flex gap-4">
           {navContent.map(({ title, url }) => (
-            <Link href={url} key={title} className="text-gray-500 hover:text-secondary duration-200">
-              <li>{title}</li>
-            </Link>
+            <li key={title} >
+              <Link href={url}>
+                <Magnetic className="text-gray-500 hover:text-secondary duration-200 p-2">
+                  {title}
+                </Magnetic>
+              </Link>
+            </li>
+
           ))}
         </ul>
       </nav>
---
+++ app/components/effect/Magnetic.tsx
@@ -0,0 +1,48 @@
+"use client"
+import { motion } from "framer-motion"
+import { useRef, useState, HTMLProps } from "react"
+
+const Magnetic = ({
+  className,
+  children
+}: {
+  className?: HTMLProps<HTMLElement>["className"]
+  children?: React.ReactNode
+}) => {
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const ref = useRef<HTMLDivElement>(null);
+
+  const mouseMove = (e: any) => {
+    const { clientX, clientY } = e;
+    const { width, height, left, top } = ref.current!.getBoundingClientRect();
+
+    const x = clientX - left - width / 2;
+    const y = clientY - top - height / 2;
+
+    setPosition({ x, y })
+  }
+
+  const mouseLeave = () => {
+    setPosition({ x: 0, y: 0 })
+  }
+
+  return (
+    <motion.div
+      ref={ref}
+      onMouseMove={mouseMove}
+      onMouseLeave={mouseLeave}
+      className={className}
+      animate={{ x: position.x, y: position.y }}
+      transition={{ type: "spring", stiffness: 150, damping: 30, mass: 0.1 }}
+    >
+      {children}
+    </motion.div>
+
+  )
+}
+
+export default Magnetic
+
+
+  // const MotionCustom = motion(CustomComponent, { forwardMotionProps: true })
