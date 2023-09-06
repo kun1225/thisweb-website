16dab9b9878e2422bd26a5114d34b6d606d9996f
thisWeb1225 <shan881225@gmail.com>
2023-09-06 17:17:10 +0800

    :sparkles: Feat: add some lib funciton

---
+++ lib/lib.ts
@@ -0,0 +1,27 @@
+import { Articles } from 'contentlayer/generated';
+import { compareDesc } from 'date-fns';
+
+export const compose1 = (...fns: Array<(x:any)=>any>) => (x: any) => fns.reduceRight((v, f) => f(v), x);
+
+type Func<T, U> = (arg: T) => U;
+
+export function compose<T>(...functions: Array<Func<T, T>>): Func<T, T> {
+  return (input: T): T => {
+    return functions.reduceRight((result, func) => func(result), input);
+  };
+}
+
+export function composeWithInitialValue<T>(
+  initialValue: T,
+  ...functions: Array<Func<T, T>>
+): T {
+  return functions.reduceRight((result, func) => func(result), initialValue);
+}
+
+export const sortArticleByDate = (allArticles: Articles[]) => {
+  return allArticles.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
+}
+
+export const filterActiveArticles = (allArticles: Articles[]) => {
+  return allArticles.filter((article) => article.isActive === true)
+}
---
+++ lib/mdxComponents.ts
@@ -0,0 +1,8 @@
+import CustomLink from "@/app/components/CustomLink";
+
+// Custom components/renderers to pass to MDX.
+const mdxComponents = {
+  a: CustomLink,
+};
+
+export default mdxComponents;
