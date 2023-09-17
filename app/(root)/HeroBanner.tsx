50535d9e9e57733a2912920d103a2967cd2f3c80
thisWeb1225 <shan881225@gmail.com>
2023-09-17 22:07:07 +0800

    :sparkles: Feat: add back newsletter

--- app/(root)/HeroBanner.tsx
+++ app/(root)/HeroBanner.tsx
@@ -22,9 +22,9 @@ const HeroBanner = () => {
       >
         <p className="text-xs font-normal tracking-widest text-gray-500">請網這邊走 ThisWeb | 前端 x 轉職 x 提升競爭力</p>
         <h2 className="font-semibold text-3xl md:text-4xl leading-tight">幫助你從零開始學習網頁程式</h2>
-        <p className="text-xs md:text-base">提供完整入門教學、JS 核心觀念講解，讓你快速上手網頁程式，學會前端核心技能，轉職更順利。</p>
-        {/* <p className="text-base">加入<span className="text-secondary">電子報</span>，每週三準時收到 1 篇前端的技術文章。</p> */}
-        {/* <Newsletter formId="5590412"/> */}
+        <p className="text-gray-500 text-xs md:text-base">提供完整入門教學、JS 核心觀念講解，讓你快速上手網頁程式，學會前端核心技能，轉職更順利。</p>
+        <p className="text-base">加入<span className="text-secondary">電子報</span>，每週三準時收到 1 篇前端的技術文章。</p>
+        <Newsletter formId="5590412"/>
       </motion.div>
 
       <motion.div
