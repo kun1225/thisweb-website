00ece3b8a4b092a862ad8df0c0fba57004a2bc81
thisWeb1225 <shan881225@gmail.com>
2023-09-24 20:38:16 +0800

    :bricks: Feat: add linting rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,18 @@ const project = resolve(__dirname, 'tsconfig.json');
 module.exports = {
   root: true,
   extends: [
-    require.resolve('@vercel/style-guide/eslint/node'),
-    require.resolve('@vercel/style-guide/eslint/browser'),
     require.resolve('@vercel/style-guide/eslint/typescript'),
+    require.resolve('@vercel/style-guide/eslint/browser'),
+    require.resolve('@vercel/style-guide/eslint/react'),
+    require.resolve('@vercel/style-guide/eslint/next'),
     'next/core-web-vitals',
-    'prettier',
   ],
   parserOptions: {
     project,
+  },
+  globals: {
+    React: true,
+    JSX: true,
   },
   settings: {
     'import/resolver': {
@@ -20,5 +24,20 @@ module.exports = {
         project,
       },
     },
+  },
+  ignorePatterns: ['node_modules/', 'dist/'],
+  rules: {
+    'unicorn/filename-case': 'off',
+    'import/order': 'off',
+    'import/no-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'react/jsx-sort-props': 'off',
+    'react/function-component-definition': 'off',
+    'no-console': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unsafe-return': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unsafe-assignment': 'warn',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
   },
 };
