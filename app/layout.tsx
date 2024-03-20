084bfcf511c386899043617c7ab002991a453b8b
thisWeb1225 <shan881225@gmail.com>
2024-03-20 22:16:04 +0800

    ✨ Feat : add  vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,9 @@ import Footer from './components/Footer';
 import ProgressBar from './components/ProgressBar';
 import { Suspense } from 'react';
 
+// Vercel Analytics
+import { Analytics } from '@vercel/analytics/react';
+
 // Metadata
 // TODO : Create generate open graph image file
 export const metadata: Metadata = {
@@ -17,15 +20,23 @@ export const metadata: Metadata = {
   description:
     '提供完整的前端入門教學，從 HTML、CSS、JS 核心觀念開始，進階到網頁動畫、前端框架以及前端工程師自我成長等內容，讓你快速上手網頁程式，奠定基礎知識，轉職升職更順利。',
   creator: 'ThisWeb 請網這邊走',
-  keywords: ['工程師自我成長', '工程師職涯競爭力', '前端最新資訊', 'JavaScript 教學', 'CSS 教學', 'HTML教學', '前端教學' ],
+  keywords: [
+    '工程師自我成長',
+    '工程師職涯競爭力',
+    '前端最新資訊',
+    'JavaScript 教學',
+    'CSS 教學',
+    'HTML教學',
+    '前端教學',
+  ],
   openGraph: {
     type: 'website',
     title: 'ThisWeb 請網這邊走 - 幫助你自學前端 ',
     description:
       '提供完整的前端入門教學，從 HTML、CSS、JS 核心觀念開始，進階到網頁動畫、前端框架以及前端工程師自我成長等內容，讓你快速上手網頁程式，奠定基礎知識，轉職升職更順利。',
     locale: 'zh-TW',
-    siteName: 'ThisWeb 請網這邊走' ,
-  }
+    siteName: 'ThisWeb 請網這邊走',
+  },
 };
 
 export const revalidate = 0;
@@ -41,9 +52,12 @@ export default function RootLayout({
         <Suspense>
           <ProgressBar />
         </Suspense>
-          <Header />
-          <main className="px-4 md:px-8 lg:px-16 xl:px-20 max-w-7xl mx-auto">{children}</main>
-          <Footer />
+        <Header />
+        <main className="px-4 md:px-8 lg:px-16 xl:px-20 max-w-7xl mx-auto">
+          {children}
+        </main>
+        <Footer />
+        <Analytics />
       </body>
     </html>
   );
