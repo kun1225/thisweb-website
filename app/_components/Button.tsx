a2071117b57a8eb8996b975cccacaaf3b26c2a29
thisWeb1225 <shan881225@gmail.com>
2024-03-22 10:06:56 +0800

    💄 Modify button style

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -1,30 +1,39 @@
-import type { HTMLProps } from 'react';
+import type { HtmlHTMLAttributes, HTMLProps } from 'react';
 
 function Button({
   className,
   children,
   type = 'button',
+  buttonType = 'primary',
+  disabled = false,
+  ...props
 }: {
   className?: HTMLProps<HTMLElement>['className'];
   children: React.ReactNode;
   type?: HTMLButtonElement['type'];
+  buttonType?: 'primary' | 'outline';
+  disabled?: boolean;
 }) {
+  const typeClass = (() => {
+    if (disabled) {
+      return 'cursor-not-allowed bg-gray-200 text-gray-400 shadow-none';
+    }
+
+    switch (buttonType) {
+      case 'primary':
+        return 'bg-secondary text-white hover:bg-secondary-2 hover:shadow-2xl hover:scale-105';
+      case 'outline':
+        return 'border-2 border-secondary text-secondary hover:shadow-2xl hover:scale-105 hover:border-transparent hover:bg-secondary-2 hover:text-white';
+      default:
+        return '';
+    }
+  })();
   return (
     <button
-      className={`
-      w-full 
-      md:w-80 
-      border-2 
-      border-secondary 
-      text-secondary
-      text-sm 
-      px-6 py-2 
-      rounded-sm
-      duration-200 
-      hover:bg-gray-200
-      hover:border-transparent
-      ${className}`}
+      className={`w-full md:w-80 text-sm px-6 py-2 shadow-md rounded-sm duration-200 ${typeClass} ${className}`}
       type={type}
+      disabled={disabled}
+      {...props}
     >
       {children}
     </button>
