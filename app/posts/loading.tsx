e1098720169ff5c0329959f57e8859942423724f
thisWeb1225 <shan881225@gmail.com>
2024-05-01 18:29:35 +0800

    🐛 Add list key

--- app/posts/loading.tsx
+++ app/posts/loading.tsx
@@ -4,8 +4,8 @@ import Stack from '../_components/Stack';
 const postsPageLoading = () => {
   return (
     <Stack direction='col' gap={8}>
-      {Array.from({ length: 3 }).map((_) => (
-        <div>
+      {Array.from({ length: 3 }).map((_, i) => (
+        <div key={i}>
           <Skeleton className="w-[800px] max-w-[80%] h-6" />
           <Skeleton className="w-32 max-w-[20%] h-3 mt-1" />
           <Skeleton className="w-[100%] h-4 mt-2" />
