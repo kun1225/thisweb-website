43bd6c1dc5d05ce181b533b0825addd21b57c250
thisWeb1225 <shan881225@gmail.com>
2024-06-04 16:46:57 +0800

    ✨ Feat : add global settings context

---
+++ app/_hook/useGlobalSettings.tsx
@@ -0,0 +1,14 @@
+import { useContext } from 'react';
+import { GlobalSettingsContext } from '../_context/globalSettings';
+
+const useGlobalSettings = () => {
+  const context = useContext(GlobalSettingsContext);
+  if (!context) {
+    throw new Error(
+      'useGlobalSettings needs to be used in GlobalSettingsProvide',
+    );
+  }
+  return context;
+};
+
+export default useGlobalSettings;
