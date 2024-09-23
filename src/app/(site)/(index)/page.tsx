99279f37accd7bca3574324b5750b7a9912f68d2
kun1225 <shan881225@gmail.com>
2024-09-23 11:31:32 +0800

    💄  Style : use scss on HomeProblems section

--- src/app/(site)/(index)/page.tsx
+++ src/app/(site)/(index)/page.tsx
@@ -1,40 +1,11 @@
 import HomeHero from './_components/HomeHero';
-import ProblemSection from '../../_components/ProblemSection';
+import HomeProblems from './_components/HomeProblems';
 import SiteOwnerSection from './_components/SiteOwnerSection';
 import RecommendedPosts from './_components/RecommendedPosts';
-
-import { AiOutlineFrown } from 'react-icons/ai';
-import { BsCodeSlash } from 'react-icons/bs';
-import { IoLanguageOutline } from 'react-icons/io5';
-import { GiBrokenWall } from 'react-icons/gi';
-
 // Sanity Utils
 import { sanityFetch } from '@/src/libs/sanity/client';
 import { LIMITED_POSTS_QUERY } from '@/src/libs/sanity/queries';
 import { PostsType } from '@/src/libs/sanity/type';
-
-const problemCardContent = [
-  {
-    icon: AiOutlineFrown,
-    title: '知識焦慮',
-    desc: '網路上太多教學，不知道從何學起',
-  },
-  {
-    icon: BsCodeSlash,
-    title: '不知道如何實作',
-    desc: '掌握基礎知識後，不知道如何應用在實際專案中',
-  },
-  {
-    icon: IoLanguageOutline,
-    title: '語言障礙',
-    desc: '中文資訊不夠完整，英文又看不是很懂',
-  },
-  {
-    icon: GiBrokenWall,
-    title: '學習遇到瓶頸',
-    desc: '擔心自己跟不上周遭同儕，覺得沒自信',
-  },
-];
 
 export default async function Home() {
   const posts = await sanityFetch<PostsType>({
@@ -49,11 +20,7 @@ export default async function Home() {
   return (
     <>
       <HomeHero />
-      <ProblemSection
-        problemTitle="如果你遇到這些問題"
-        problemCardContent={problemCardContent}
-        className="c my-24"
-      />
+      <HomeProblems />
       <SiteOwnerSection />
       <RecommendedPosts limitedPosts={posts} />
     </>
