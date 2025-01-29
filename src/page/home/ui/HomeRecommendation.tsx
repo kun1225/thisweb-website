cc5804ad70bbb28f51caf665f66cb51c1bdb9d72
kun1225 <kun881225@gmail.com>
2025-01-29 13:06:33 +0800

    feat : improve HomeRecommendation style

--- src/page/home/ui/HomeRecommendation.tsx
+++ src/page/home/ui/HomeRecommendation.tsx
@@ -10,19 +10,21 @@ export function HomeRecommendation({ data }: { data: TypeHome['recommendation'] 
   const { heading, headingId, subheading, paragraph, media, cta } = data;
 
   return (
-    <section className="flex flex-col items-center bg-sky-50 px-edge-dynamic py-32 text-center">
-      <HomeSubheading subheading={subheading} />
-      <HomeHeading heading={heading} headingId={headingId} />
+    <section className="px-edge-dynamic py-16 text-center">
+      <div className="flex flex-col items-center rounded-3xl bg-sky-50 px-4 py-16 shadow-lg">
+        <HomeSubheading subheading={subheading} />
+        <HomeHeading heading={heading} headingId={headingId} />
 
-      <Media data={media} className="mb-8 max-w-xl" />
+        <Media data={media} className="mb-8 max-w-xl" />
 
-      <div className="mb-8 text-pretty prose-p:mb-1 prose-ol:mb-2 prose-ol:mt-4 prose-ol:list-inside prose-ol:list-decimal prose-ul:mb-2">
-        <CustomPortableText value={paragraph} />
+        <div className="mb-8 text-pretty prose-p:mb-1 prose-ol:mb-2 prose-ol:mt-4 prose-ol:list-inside prose-ol:list-decimal prose-ul:mb-2">
+          <CustomPortableText value={paragraph} />
+        </div>
+
+        <Button asChild>
+          <Link href={cta?.url}>{cta?.label}</Link>
+        </Button>
       </div>
-
-      <Button asChild>
-        <Link href={cta?.url}>{cta?.label}</Link>
-      </Button>
     </section>
   );
 }
