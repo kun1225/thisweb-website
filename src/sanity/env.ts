ff2e7b865b27c6a13975c69498058879efe40e91
kun1225 <kun881225@gmail.com>
2025-10-13 17:13:10 +0800

    refactor: enhance dataset selection logic based on environment and branch

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,9 +1,13 @@
+const isProd = process.env.NODE_ENV === 'production';
+const branch = process.env.VERCEL_GIT_COMMIT_REF ?? '';
+
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-09-01';
 
-export const dataset =
-  process.env.NODE_ENV === 'production'
-    ? process.env.NEXT_PUBLIC_SANITY_DATASET
-    : process.env.NEXT_PUBLIC_SANITY_DATASET_DEV;
+export const dataset = isProd
+  ? branch === 'dev'
+    ? process.env.NEXT_PUBLIC_SANITY_DATASET_DEV
+    : process.env.NEXT_PUBLIC_SANITY_DATASET
+  : process.env.NEXT_PUBLIC_SANITY_DATASET_DEV;
 
 // export const dataset = assertValue(
 //   process.env.NEXT_PUBLIC_SANITY_DATASET,
