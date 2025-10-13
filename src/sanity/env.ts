92cced9430c7b052d4de255e666560cc002b476a
kun1225 <kun881225@gmail.com>
2025-10-13 17:17:43 +0800

    refactor: improve dataset environment variable assertion for clarity

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -3,16 +3,14 @@ const branch = process.env.VERCEL_GIT_COMMIT_REF ?? '';
 
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-09-01';
 
-export const dataset = isProd
-  ? branch === 'dev'
-    ? process.env.NEXT_PUBLIC_SANITY_DATASET_DEV
-    : process.env.NEXT_PUBLIC_SANITY_DATASET
-  : process.env.NEXT_PUBLIC_SANITY_DATASET_DEV;
-
-// export const dataset = assertValue(
-//   process.env.NEXT_PUBLIC_SANITY_DATASET,
-//   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-// );
+export const dataset = assertValue(
+  isProd
+    ? branch === 'dev'
+      ? process.env.NEXT_PUBLIC_SANITY_DATASET_DEV
+      : process.env.NEXT_PUBLIC_SANITY_DATASET
+    : process.env.NEXT_PUBLIC_SANITY_DATASET_DEV,
+  'Missing dataset env variable'
+);
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
